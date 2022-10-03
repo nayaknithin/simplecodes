96ab78c54a0673fd60d9bacd78556ee8f9e7c9f5
nayaknithin <44192441+nayaknithin@users.noreply.github.com>
2022-10-03 22:07:44 +0530

    update ShinySetup

--- MSTAT/R/ShinySetup.R
+++ MSTAT/R/ShinySetup.R
@@ -3,39 +3,42 @@
 #' @param path The directory path where you want to setup the Shiny app folder; defaults to your current working directory.
 #' @description This function help setup directories and files for new Shiny development.
 #' @export
-
 ShinySetup=function(path=getwd()){
+  cat(cli::rule(
+    left = crayon::bold("Creating Sub-Directories"),
+    right = Sys.time()
+  ),"\n")
   folders=c("R","www")
-for (i in 1:length(folders)){
-  if(dir.exists(paste0(path,"/",folders[i],sep=""))){cat("\n xxxxxxxxxxxxxxx\n Folder ",folders[i],"already exits...\n Skipping this folder...\n \n")
-}else{
-    dir.create(paste0(path,"/",folders[i],sep=""))
-    cat("\n===============\n Folder",folders[i]," is sucessfully created.")
+  for (i in 1:length(folders)){
+    if(dir.exists(paste0(path,"/",folders[i],sep=""))){cat(crayon::red(cli::symbol$cross)," Folder",paste0("~/",folders[i],sep="")," already exits...\n Skipping this folder...\n")
+    }else{
+      dir.create(paste0(path,"/",folders[i],sep=""))
+      cat( crayon::green(cli::symbol$tick)," Folder ",paste0("~/",folders[i],sep="")," is sucessfully created.\n")
     }
-}
+  }
+  cat(cli::bg_br_green("Folder Generation Complete"),"\n",crayon::bold(paste0("The Created Folders are in:",crayon::cyan(path),sep="")),"\n \n \n")
+  cat( cli::rule(
+    left = "Creating Scripts",
+    right = Sys.time()
+  ),"\n")
 
-cat("\n+++++++++++++++\n Folder Generation Complete.\n \n Initiating Scripts Generation.")
-
-Scripts=c("global","ui","server","app")
-  app="\n shinyApp(ui=ui,server=server)\n"
-  ui="\n ui <- fluidpage(\n \n }\n"
+  Scripts=c("global","ui","server","app")
+  app="shinyApp(ui=ui,server=server)\n"
+  ui="ui <- fluidPage(\n \n )\n"
   server="server <- funcion(input,output,session){\n \n }\n"
   global="# Packages Required -------------------------------------------------------
 \n library(shiny) \n #add additional packages required here...."
 
   for (i in 1:length(Scripts)) {
     filename=paste0(path,"/",Scripts[i],".R",sep="")
-    if(file.exists(filename)){cat("\n xxxxxxxxxxxxxxx\n The File ",paste0(Scripts[i],".R",sep=""),"already exits...\n Skipping this file...\n \n")
-      }else{
-    sink(file=filename,append=T)
-    cat(eval(parse(text=Scripts[i])))
-    sink()
-    cat("\n===============\n",paste0(Scripts[i],".R",sep="") ,"is sucessfully generated.")
-      }
+    if(file.exists(filename)){cat(crayon::red(cli::symbol$cross)," ",paste0(Scripts[i],".R",sep=""),"already exits...\n Skipping this file...\n")
+    }else{
+      sink(file=filename,append=T)
+      cat(eval(parse(text=Scripts[i])))
+      sink()
+      cat(crayon::green(cli::symbol$tick)," ",paste0(Scripts[i],".R",sep="") ,"is sucessfully generated.\n")
+    }
   }
 
-cat("\n=====Process Completed=====\n")
+  cat(cli::bg_br_green("Script Generation Complete"),"\n", crayon::bold(paste0("The Created Scripts are in:",crayon::cyan(path),sep="")),"\n \n \n")
 }
-
-
-
